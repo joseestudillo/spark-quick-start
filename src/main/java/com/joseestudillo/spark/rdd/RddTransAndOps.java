f25ecb394e8784ed537ce7292124edc7dcd04f31
Jose Estudillo <estudillo.jose@gmail.com>
2015-08-03 00:58:18 +0100

    Rdd operations examples added

--- src/main/java/com/joseestudillo/spark/rdd/RddTransAndOps.java
+++ src/main/java/com/joseestudillo/spark/rdd/RddTransAndOps.java
@@ -1,14 +1,15 @@
 package com.joseestudillo.spark.rdd;
 
+import java.util.Arrays;
 import java.util.List;
-import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-
-import scala.actors.threadpool.Arrays;
+import org.apache.spark.api.java.function.FlatMapFunction;
 
 import com.joseestudillo.spark.SparkTextSearch;
 import com.joseestudillo.spark.utils.SparkUtils;
@@ -21,32 +22,58 @@ public class RddTransAndOps {
 		SparkConf conf = SparkUtils.getLocalConfig(SparkTextSearch.class.getSimpleName());
 		log.info("access to the web interface at localhost:4040");
 		JavaSparkContext spark = new JavaSparkContext(conf);
+		
+		// # Load info from text files
 		String filepath = SparkUtils.getClasspathFileFullPath("numbers.txt");
+		JavaRDD<String> stringsRdd;
+		stringsRdd = spark.textFile(filepath);
 		
+		// # Loading information in a collection to spark
+		List<Integer> integers = Stream.iterate(0, n -> n + 1).limit(10).collect(Collectors.toList());
+		JavaRDD<Integer> intsRdd = spark.parallelize(integers);
+		log.info(String.format("Input integers collection: %s", intsRdd));
 		
-		JavaRDD<String> textFileRdd = spark.textFile(filepath);
-		log.info(textFileRdd.collect());
-		JavaRDD<Integer> intFileRdd = textFileRdd.map((s) -> Integer.valueOf(s));
-		log.info(intFileRdd.collect());
-		Integer reduction = intFileRdd.reduce((a,b) -> a + b);
-		JavaRDD<String> union = textFileRdd.union(intFileRdd.map(i -> Integer.toString(i)));
+		// # map
+		stringsRdd = intsRdd.map((i) -> Integer.toString(i)); 
+		log.info(String.format("maps the integers into strings", stringsRdd.collect()));
 		
-		// #map
-		
-		// #flatMap
-		
-		
-		log.info(union.collect());
-		// #sortBy
-		//for sort by what you need is a function that give you a value to sort by, in this case we will use the string itself
-		Function<String,String> functor = x -> x;  
-		log.info(union.distinct().sortBy(functor,true,1).collect());
-		Function<String,Integer> functor2 = x -> Integer.valueOf(x);
-		log.info(union.distinct().sortBy(functor2,true,1).collect());
+		// # reduction
+		Integer intReduction = intsRdd.reduce((a,b) -> a + b);
+		String stringReduction = stringsRdd.reduce((a,b) -> a + b);
+		log.info(String.format("Integers %s reduced using + to %s", intsRdd.collect(), intReduction));
+		log.info(String.format("Strings %s reduced using + to %s", stringsRdd.collect(), stringReduction));
 		
 		// #union
+		JavaRDD<Integer> intsRdd2 = spark.parallelize(Stream.iterate(0, n -> n + 1).limit(5).collect(Collectors.toList()));
+		JavaRDD<Integer> unionRdd = intsRdd.union(intsRdd2);
+		log.info(String.format("Union(%s, %s): %s", intsRdd.collect(), intsRdd2.collect(), unionRdd.collect()));
+		
 		// #intersection
+		log.info(String.format("Intersection(%s, %s): %s", intsRdd.collect(), intsRdd2.collect(), intsRdd.intersection(intsRdd2).collect()));
+		
 		// #substract
+		log.info(String.format("Substract(%s, %s): %s", intsRdd.collect(), intsRdd2.collect(), intsRdd.subtract(intsRdd2).collect()));
+		
 		// #cartesian
+		log.info(String.format("Cartersian(%s, %s): %s", intsRdd.collect(), intsRdd2.collect(), intsRdd.cartesian(intsRdd2).collect()));
+		
+		// #sortBy
+		log.info(String.format("Unsorted union result: %s", unionRdd.collect()));
+		//for sortBy what you need is a function that give you a value to sort by, in this case we will use the string itself
+		JavaRDD<Integer> sortedRdd = unionRdd.sortBy(x -> x,true,1); 
+		log.info(String.format("Collection sorted %s", sortedRdd.collect()));
+		
+		// #distinct
+		log.info(String.format("Removing repeated elements: %s Notice that 'distinct' mess up the sorted rdd", sortedRdd.distinct().collect()));
+		
+		// #flatMap
+		JavaRDD<List<Integer>> listOfLists = intsRdd.map(x -> Arrays.asList(x, x));
+		
+		org.apache.spark.api.java.function.Function<List<Integer>, ?> mapper = x -> Arrays.asList(x.stream().reduce(Integer::sum).get());
+		FlatMapFunction<List<Integer>, ?> flatMapper = x -> Arrays.asList(x.stream().reduce(Integer::sum).get());
+		//notice that mapper and flatMapper are the same function, they just cant be casted to each other.
+		log.info(String.format("to map: %s", listOfLists.collect()));
+		log.info(String.format("map sum(List<Int>): %s", listOfLists.map(mapper).collect()));
+		log.info(String.format("flatMap sum(List<Int>): %s", listOfLists.flatMap(flatMapper).collect()));
 	}
 }
